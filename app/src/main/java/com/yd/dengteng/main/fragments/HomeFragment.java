0c7e8ecea40fe85a1a2e1d069586b78231db7576
wcx1013 <2744729611@qq.com>
2022-01-11 17:24:43 +0800

    Initial commit

---
+++ app/src/main/java/com/yd/dengteng/main/fragments/HomeFragment.java
@@ -0,0 +1,30 @@
+package com.yd.dengteng.main.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.xzq.module_base.base.BasePresenterFragment;
+import com.yd.dengteng.R;
+
+/**
+ * HomeFragment
+ * Created by xzq on 2020/8/4.
+ */
+public class HomeFragment extends BasePresenterFragment {
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected int getLayoutId(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return R.layout.fragment_home;
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/com/yd/dengteng/main/fragments/MeFragment.java
@@ -0,0 +1,30 @@
+package com.yd.dengteng.main.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.xzq.module_base.base.BasePresenterFragment;
+import com.yd.dengteng.R;
+
+/**
+ * MeFragment
+ * Created by xzq on 2020/8/4.
+ */
+public class MeFragment extends BasePresenterFragment {
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected int getLayoutId(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return R.layout.fragment_me;
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
+    }
+}
